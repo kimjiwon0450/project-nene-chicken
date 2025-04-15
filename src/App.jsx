effd48429cae53d5d2c4992e0fd3031360ba23bd
youthdonut <dbtmdgus14@naver.com>
2025-04-15 17:23:56 +0900

    1차합

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import ChickenFooter from './components/ChickenFooter';
 import ChickenNavBar from './components/ChickenNavBar';
-import React from 'react';
+import React from 'react'; // eslint-disable-line no-unused-vars
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Banner from './components/Banner';
 import MenuSection from './components/MenuSection';
--- src/BrandStartUp/BrandStartUp.jsx
+++ src/BrandStartUp/BrandStartUp.jsx
@@ -1,11 +1,13 @@
 import React from 'react'; // eslint-disable-line no-unused-vars
 import BrandStartUpFooter from './BrandStartUp/BrandStartUpFooter';
 import BransStartupHeader from './BrandStartUp/BransStartupHeader';
+import IntroduceBrand from '../components/IntroduceBrand';
 
 const BrandStartUp = () => {
   return (
     <>
       <BransStartupHeader />
+      <IntroduceBrand />
       <BrandStartUpFooter />
     </>
   );
