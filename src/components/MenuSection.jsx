0fb318570c17928dfbf82310eaa20c75546b758f
kimjiwon0450 <fresh04502@naver.com>
2025-04-15 12:37:38 +0900

    메인 Body 내용 채워넣기기

---
+++ src/components/MenuSection.jsx
@@ -0,0 +1,127 @@
+import React, { useState } from 'react';
+import './MenuSection.css';
+
+const products = [
+  {
+    id: 1,
+    name: '후라이드',
+    description: '겉은 바삭하고 속은 부드러운 네네치킨 대표 메뉴',
+    price: '19,000원',
+    image: '/30001.jpg',
+  },
+  {
+    id: 2,
+    name: '오리엔탈파닭',
+    description: '새콤달콤 짭조름한 오리엔탈 소스에 신선한 파가 듬뿍',
+    price: '22,000원',
+    image: '/30063.jpg',
+  },
+  {
+    id: 3,
+    name: '양념치킨',
+    description: '매콤달콤한 맛의 네네치킨 대표 양념치킨',
+    price: '21,000원',
+    image: '/30013.jpg',
+  },
+  {
+    id: 4,
+    name: '치즈스노윙',
+    description: '입 안에서 사르르 녹는 스노윙 치즈',
+    price: '22,000원',
+    image: '/30038.jpg',
+  },
+  {
+    id: 5,
+    name: '스노윙MAXX',
+    description: '매콤달콤한 스노윙MAXX치킨과 달콤향긋한 찍어바요딥핑소스의 완벽한 조화',
+    price: '23,000원',
+    image: '/6086.jpg',
+  },
+  {
+    id: 6,
+    name: '청양마요치킨',
+    description: '아삭한 양파와 매콤한 청양마요소스 맛의 대조화!',
+    price: '22,000원',
+    image: '/3813.jpg',
+  },
+  {
+    id: 7,
+    name: '핫후라이드',
+    description: '겉은 바삭하고 속살은 부드러운 네네치킨 대표메뉴',
+    price: '20,000원',
+    image: '/30002.jpg',
+  },
+  {
+    id: 8,
+    name: '닭다리(10개)',
+    description: '푸짐하고 쫄깃한 닭다리로 만든 후라이드 닭다리',
+    price: '23,000원',
+    image: '/30005.jpg',
+  },
+  {
+    id: 9,
+    name: '소이갈릭치킨',
+    description: '짭조름한 소이갈릭치킨',
+    price: '22,000원',
+    image: '/30415.jpg',
+  },
+  {
+    id: 10,
+    name: '콤보(닭다리반(5개)+날개반(6개))',
+    description:
+      '닭다리의 담백함과 날개의 바삭함을 한번에! 두 가지 매력을 동시에 맛볼 수 있는 완벽한 치킨 메뉴',
+    price: '23,000원',
+    image: '/1030.jpg',
+  },
+];
+
+const VISIBLE_COUNT = 5;
+
+const MenuSection = () => {
+  const [startIndex, setStartIndex] = useState(0);
+
+  const handlePrev = () => {
+    setStartIndex((prev) => (prev === 0 ? 0 : prev - 1));
+  };
+
+  const handleNext = () => {
+    setStartIndex((prev) => (prev + VISIBLE_COUNT >= products.length ? prev : prev + 1));
+  };
+
+  return (
+    <div className='MenuSlider'>
+      <div className='slider-container'>
+        <h2 className='title'>BEST</h2>
+        <div className='slider-wrapper'>
+          <button onClick={handlePrev} className='nav-button'>
+            ←
+          </button>
+          <div className='slider-window'>
+            <div
+              className='slider-track'
+              style={{
+                transform: `translateX(-${startIndex * 200}px)`,
+              }}
+            >
+              {products.map((product, index) => (
+                <div className='product-card' key={index}>
+                  <img src={product.image} alt={product.name} />
+                  <div className='MenuInfo'>
+                    <h3>{product.name}</h3>
+                    <p className='description'>{product.description}</p>
+                    <p className='price'>{product.price.toLocaleString()}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <button onClick={handleNext} className='nav-button'>
+            →
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MenuSection;
---
+++ src/components/ServiceBoxes.jsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import './ServiceBoxes.css';
+
+function ServiceBoxes() {
+  return (
+    <>
+      <div className='SubBannerWrap'>
+        <div className='SubBanner'>
+          <div className='SbanBox color1 Cpointer'>
+            <span>
+              가까운 네네치킨 매장을
+              <br /> 찾아보세요
+            </span>
+            <img src='/find-store.png' className='find-store' alt='네네치킨 매장찾기' />
+          </div>
+          <div className='SbanBox color2 Cpointer'>
+            <span>
+              모바일교환권/금액권으로 마음을
+              <br /> 선물하세요
+            </span>
+            <img src='/gift.png' className='gift' alt='선물하기' />
+          </div>
+          <div className='SbanBox color3 Cpointer'>
+            <span>
+              다양한 이벤트 재미를
+              <br /> 경험해보세요!
+            </span>
+            <img src='/event.png' className='event' alt='이벤트정보보기' />
+          </div>
+        </div>
+      </div>
+
+      <div className='SubProcess'>
+        <div className='ProcessBox'>
+          <div className='ProcessList'>
+            <div>
+              <img src='/delivery.png' alt='배달주문' />
+            </div>
+            <div className='PS_title'>
+              <div className='tBox'>
+                <div style={{ flex: 1 }}>
+                  <span className='PStype'>배달</span>주문
+                </div>
+                <span className='ticon' />
+                <img src='/icon_subLeft.svg' alt='' />
+              </div>
+            </div>
+            <div className='PS_Stitle'>원하는 곳에서 받아 보세요</div>
+          </div>
+
+          <div className='ProcessList'>
+            <div>
+              <img src='/take-out.png' alt='포장주문' />
+            </div>
+            <div className='PS_title'>
+              <div className='tBox'>
+                <div style={{ flex: 1 }}>
+                  <span className='PStype'>포장</span>주문
+                </div>
+                <span className='ticon' />
+                <img src='/icon_subLeft.svg' alt='' />
+              </div>
+            </div>
+          </div>
+
+          <div className='ProcessList quick'>
+            <div>
+              <img src='/quick-order.png' alt='퀵오더' />
+            </div>
+            <div className='PS_title'>
+              <div className='tBox'>
+                <div style={{ flex: 1 }}>
+                  <span className='PStype'>퀵</span>오더
+                </div>
+                <span className='ticon' />
+                <img src='/icon_subLeft.svg' alt='' />
+              </div>
+            </div>
+            {/* PS_title 닫는 태그 추가 */}
+            <div className='PS_Stitle'>
+              자주 주문하는 메뉴를 <br />
+              빠르게 주문해요
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id='btnTop'>
+        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 6.5 3.79' />
+      </div>
+    </>
+  );
+}
+
+export default ServiceBoxes;
