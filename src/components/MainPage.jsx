5fe5473223fa62c6e3d7bd7ecc713768b249be18
kimjiwon0450 <fresh04502@naver.com>
2025-04-15 17:29:37 +0900

    파일 병합 및 라우터 1차 커밋

---
+++ src/components/MainPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Banner from './Banner';
+import ServiceBoxes from './ServiceBoxes';
+import MenuSection from './MenuSection';
+
+const MainPage = () => {
+  return (
+    <div>
+      <Banner />
+      <ServiceBoxes />
+      <MenuSection />
+    </div>
+  );
+};
+
+export default MainPage;
