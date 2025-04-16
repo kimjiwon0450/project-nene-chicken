efbce5ab451474a1ff4073ebf01776c4266fd8cc
kimjiwon0450 <fresh04502@naver.com>
2025-04-16 10:44:05 +0900

    빌드를 위한 이미지 경로 수정정

--- src/routes/router-config.jsx
+++ src/routes/router-config.jsx
@@ -9,34 +9,38 @@ import { LoginProvider } from '../context/loginContext';
 import { useEffect } from 'react';
 
 // 라우터 설정
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <RootLayout />,
+      // errorElement: <ErrorPage />,
+      children: [
+        {
+          index: true, // 부모 경로가 활성화 되었을 때 기본으로 사용할 컴포넌트
+          element: <MainPage />,
+        },
+        {
+          path: 'login',
+          element: <LoginPage />,
+        },
+        {
+          path: 'mypage',
+          element: <Mypage />,
+        },
+      ],
+    },
+    {
+      path: '/brand_startup',
+      element: <BrandStartUp />,
+    },
+  ],
   {
-    path: '/',
-    element: <RootLayout />,
-    // errorElement: <ErrorPage />,
-    children: [
-      {
-        index: true, // 부모 경로가 활성화 되었을 때 기본으로 사용할 컴포넌트
-        element: <MainPage />,
-      },
-      {
-        path: 'login',
-        element: <LoginPage />,
-      },
-      {
-        path: 'mypage',
-        element: <Mypage />,
-      },
-    ],
+    basename: '/project-nene-chicken',
   },
-  {
-    path: '/brand_startup',
-    element: <BrandStartUp />,
-  },
-]);
+);
 
 export default function Router() {
-  
   return (
     <LoginProvider>
       <RouterProvider router={router} />
